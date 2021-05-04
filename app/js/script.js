b1bfc070e5a942677e8437eb1717cd2140f6a5b8
Ivan Kuzmanovic <ivankuzmanovic87@gmail.com>
2021-05-05 00:12:10 +0200

    full navbar functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log('connected')
+const btnMenu = document.querySelector('#btnMenu');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnMenu.addEventListener('click', () => {
+    console.log('menu button clicked')
+
+    if (header.classList.contains('open')) {
+        header.classList.remove('open');
+        btnMenu.style.opacity = '1';
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else {
+        header.classList.add('open');
+        btnMenu.style.opacity = '0';
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+})
