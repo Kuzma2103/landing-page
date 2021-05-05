c8fa144cc73c4e7f8d5234d4f53295fbc392b2af
Ivan Kuzmanovic <ivankuzmanovic87@gmail.com>
2021-05-05 16:47:25 +0200

    header, hero, features sections added

--- app/js/script.js
+++ app/js/script.js
@@ -2,18 +2,31 @@ const btnMenu = document.querySelector('#btnMenu');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 
-btnMenu.addEventListener('click', () => {
-    console.log('menu button clicked')
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
-    if (header.classList.contains('open')) {
-        header.classList.remove('open');
-        btnMenu.style.opacity = '1';
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
-    } else {
-        header.classList.add('open');
-        btnMenu.style.opacity = '0';
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
-    }
-})
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => tabContent.classList.remove('active'));
+    tabs.forEach((tab) => tab.classList.remove('active'));
+    tab.classList.add('active');
+    target.classList.add('active');
+  });
+});
+
+btnMenu.addEventListener('click', () => {
+  console.log('menu button clicked');
+
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+    btnMenu.style.opacity = '1';
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } else {
+    header.classList.add('open');
+    btnMenu.style.opacity = '0';
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
+});
